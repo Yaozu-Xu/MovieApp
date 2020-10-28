cc7992301e3d15fc68dfb324386a44427c3973f1
Yaozu-Xu <2589231996@qq.com>
2020-10-28 12:01:51 +0000

    Movie Details  page components

---
+++ src/components/headerMovie/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '../../globals/fontawesome';
+
+const MovieHeader = ({ movie }) => (
+  <div className="row">
+    <div className="col-6 offset-3">
+      <h2>
+        {movie.title}
+        {'  '}
+        <a href={movie.homepage}>
+          <FontAwesomeIcon icon={['fas', 'home']} size="1x" />
+        </a>
+      </h2>
+    </div>
+  </div>
+);
+
+export default MovieHeader;
