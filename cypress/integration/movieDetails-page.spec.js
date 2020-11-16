9a12308b286f2e77b4673b327fd53b3ff959daa8
Yaozu-Xu <2589231996@qq.com>
2020-11-16 10:35:58 +0000

    Fix: Fix test cases bugs

--- cypress/integration/movieDetails-page.spec.js
+++ cypress/integration/movieDetails-page.spec.js
@@ -2,8 +2,37 @@ let movieId = null
 let movie;
 let reviews;
 describe("Movie Details Page", () => {
-  cy.visit(`/`);
-  cy.get(".card").eq(2).find("img").click();
+
+  before(() => {
+    cy.request(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+        "TMDB_KEY"
+      )}&language=en-US&include_adult=false&include_video=false&page=1`
+    )
+      .its("body")
+      .then((response) => {
+        return response.results[2].id;
+      })
+      .then((arbitraryMovieIdignored) => {
+        movieId = arbitraryMovieIdignored
+        return cy
+          .request(
+            `https://api.themoviedb.org/3/movie/${movieId}?api_key=${Cypress.env(
+              "TMDB_KEY"
+            )}`
+          )
+          .its("body");
+      })
+      .then((movieDetails) => {
+        movie = movieDetails;
+        return movieDetails.id;
+      })
+  });
+
+  beforeEach(() => {
+    cy.visit(`/`);
+    cy.get(".card").eq(2).find("img").click();
+  });
 
   it("should display the Home icon with the correct URL value", () => {
     cy.get(".fa-home")
