84a1ce7c5e9eaab4a93e08842c887b19a4e84cbe
Yaozu-Xu <2589231996@qq.com>
2020-11-16 10:16:23 +0000

    CI - added another fake job X

--- cypress/integration/movieDetails-page.spec.js
+++ cypress/integration/movieDetails-page.spec.js
@@ -4,17 +4,17 @@ let reviews;
 describe("Movie Details Page", () => {
   cy.visit(`/`);
   cy.get(".card").eq(2).find("img").click();
-});
 
-it("should display the Home icon with the correct URL value", () => {
-  cy.get(".fa-home")
-    .parent()
-    .should("have.attr", "href")
-    .should("include", movie.homepage);
-});
-
-it("should display the poster image correctly", () => {
-cy.get("img")
-  .should("have.attr", "src")
-  .should("include", movie.poster_path)
+  it("should display the Home icon with the correct URL value", () => {
+    cy.get(".fa-home")
+      .parent()
+      .should("have.attr", "href")
+      .should("include", movie.homepage);
+  });
+  
+  it("should display the poster image correctly", () => {
+  cy.get("img")
+    .should("have.attr", "src")
+    .should("include", movie.poster_path)
+  });  
 });
