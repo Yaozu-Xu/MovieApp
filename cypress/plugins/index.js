5c7a2704bf1b175e19028e57daf955b05c066bdd
Yaozu-Xu <2589231996@qq.com>
2020-11-21 09:46:34 +0000

    Build: Add code coverage for cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,7 +15,9 @@
 /**
  * @type {Cypress.PluginConfig}
  */
+const codeCoverageTask = require("@cypress/code-coverage/task");
+
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+  codeCoverageTask(on, config);
+  return config;
+};
