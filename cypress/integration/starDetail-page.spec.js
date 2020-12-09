9cbea8ce8e3aca9861ff8753bde2833bf85d6084
Yaozu-Xu <2589231996@qq.com>
2020-12-09 22:51:41 +0000

    Fix: Fix cross-origin bug in gitlab

--- cypress/integration/starDetail-page.spec.js
+++ cypress/integration/starDetail-page.spec.js
@@ -1,4 +1,4 @@
-describe("Star page", () => {
+describe("Star Detail page", () => {
     before(() => {
         cy.configureLayoutInspector({
             excludePadding: true,
@@ -8,17 +8,24 @@ describe("Star page", () => {
     beforeEach(() => {
         cy.visit(`/`);
         cy.get("nav").find("li").eq(3).find("a").click();
-        cy.getCypressElement('poster').eq(0).click()
+        cy.wait(500)
     });
 
     describe("Layout", () => {
         it('left btn should fixed at the left ', () => {
+            cy.getCypressElement('star-name').eq(0).click()
             cy.getCypressElement('left-btn').should('be.rightOf', '.right-btn')
+        })
+        it('should naviagate to next page', () => {
+            cy.getCypressElement('star-name').eq(0).click()
+            cy.getCypressElement('right-btn').click({force: true})
+            cy.url().should('include', '/people')
         })
     })
 
     describe('Star card', () => {
         it('should render properly', () => {
+            cy.getCypressElement('star-name').eq(0).click()
             cy.getCypressElement('card').invoke('outerWidth').should('eq', 240);
         })
     })
