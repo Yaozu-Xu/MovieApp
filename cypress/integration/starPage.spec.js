9d7c368c98406e8037c65863d40bf0a2b8df1801
Yaozu-Xu <2589231996@qq.com>
2020-12-16 21:41:15 +0000

    Test: Refactor test of star pages

--- cypress/integration/starPage.spec.js
+++ cypress/integration/starPage.spec.js
@@ -1,8 +1,22 @@
+const page = 1
+let starsList
+let specifiedStar
+
 describe("Star page", () => {
     before(() => {
         cy.configureLayoutInspector({
             excludePadding: true,
         });
+        cy.request(
+                `https://api.themoviedb.org/3/person/popular?api_key=${Cypress.env(
+                    "TMDB_KEY"
+                  )}&language=en-US&page=${page}`
+            )
+            .its("body")
+            .then((response) => {
+                starsList = response.results
+                specifiedStar = starsList[0]
+            })
     })
 
     beforeEach(() => {
@@ -16,6 +30,25 @@ describe("Star page", () => {
         })
         it('right pagniation button should be fixed right', () => {
             cy.get('.pagenation-btn-right').should('be.rightOf', '.container-name');
+        })
+    })
+
+    describe('Render api data', () => {
+        it('should render stars list', () => {
+            cy.getCypressElement('star-list').should('have.length', starsList.length)
+        })
+    })
+
+    describe('Specfic star', () => {
+        it('should render specfied star properly', () => {
+            cy.getCypressElement('star-name').eq(0).should('have.text', specifiedStar.name)
+        })
+        it('should load the profile properly', () => {
+            cy.getCypressElement('star-profile').eq(0).invoke('attr', 'src').should('include', specifiedStar.profile_path)
+        })
+        it('should load the known_for movies properly', () => {
+            const selector = 'known-for-' + specifiedStar.id
+            cy.getCypressElement(selector).should('have.length', specifiedStar.known_for.length)
         })
     })
 })
